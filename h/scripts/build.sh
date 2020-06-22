f33ef045afa208365e8e2985ed0b885d5daa804e
rlnsy <rowlindsay@gmail.com>
2020-06-22 14:27:28 -0700

    modify build scripts

--- h/scripts/build.sh
+++ h/scripts/build.sh
@@ -1,5 +1,4 @@
 rm -rf _site _cache
-stack build
-stack exec site build
+cabal new-run site build
 cp -r fontawesome _site/
 echo "Done!"
--- h/scripts/run.sh
+++ h/scripts/run.sh
@@ -1,5 +1,4 @@
 rm -rf _site _cache
-stack build
-stack exec site build
+cabal new-run site build
 cp -r fontawesome _site/
-stack exec site watch
+cabal new-run site watch
